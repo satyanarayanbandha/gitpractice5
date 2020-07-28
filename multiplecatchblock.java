9585e6e02d5dbc956b0fbf57eed1fe51dd44c776
Satyanarayan Bandha <satyanarayanbandha3@gmail.com>
2020-07-28 11:24:53 +0530

    new catch code

---
+++ multiplecatchblock.java
@@ -0,0 +1,16 @@
+try
+  {
+// Protected code
+  }
+catch (ExceptionType1 e1)
+  {
+// Catch block
+  }
+catch (ExceptionType2 e2)
+  {
+// Catch block
+  }
+catch (ExceptionType3 e3)
+  {
+// Catch block
+  }
