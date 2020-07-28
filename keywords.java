bffe347d1c279289c0f80bc9be69faf18baf163f
Satyanarayan Bandha <satyanarayanbandha3@gmail.com>
2020-07-28 11:35:26 +0530

    new key code

---
+++ keywords.java
@@ -0,0 +1,8 @@
+package com.dataflair.multithreadingprogram;
+public class ThrowThrowsProgram {
+  public void deposit(double amount) throws RemoteException {
+    // Method implementation
+    throw new RemoteException();
+  }
+  // Remainder of class definition
+}
